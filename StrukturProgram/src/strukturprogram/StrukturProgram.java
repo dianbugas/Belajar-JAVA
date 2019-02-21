9abb535d856e780105ee0100affe3f0e8e965a7a
dianynf <dianbugas@gmail.com>
2019-02-21 22:17:52 +0700

    Variabel

---
+++ StrukturProgram/src/strukturprogram/StrukturProgram.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strukturprogram;
+
+/**
+ *
+ * @author dian
+ */
+public class StrukturProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("selamat datang");
+        int a =10;
+        System.out.println("niilai a " + a);
+        a = 5;
+        System.out.println("nilai a baru" + a);
+        int b = 30;
+        int c;
+        c=a*b;
+        System.out.println("A x B = " + c);
+    }
+    
+}
