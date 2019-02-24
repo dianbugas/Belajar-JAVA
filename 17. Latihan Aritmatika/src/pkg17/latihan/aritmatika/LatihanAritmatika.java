32243e6bb8d29019a07d8e5f4aa5d291f68b2235
dianynf <dianbugas@gmail.com>
2019-02-24 22:03:50 +0700

    Latihan Aritmatika

---
+++ 17. Latihan Aritmatika/src/pkg17/latihan/aritmatika/LatihanAritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg17.latihan.aritmatika;
+import java.util.Scanner;
+/**
+ *
+ * @author dian
+ */
+public class LatihanAritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int panjang, lebar, luas, tinggi, volume;
+        // Membuat perhitungan menghitung luas persegi panjang
+        // luas = panjang * lebar
+
+        System.out.println("MENGHITUNG LUAS");
+        System.out.print("panjang = ");
+        panjang = userInput.nextInt();
+        System.out.print("lebar = ");
+        lebar = userInput.nextInt();
+
+        luas = panjang * lebar;
+        System.out.println("Luas = " + luas);
+
+        System.out.println("MENGHITUNG VOLUME");
+        System.out.print("tinggi = ");
+        tinggi = userInput.nextInt();
+        volume = luas*tinggi;
+        System.out.println("volume = "+ volume);
+    }
+    
+}
