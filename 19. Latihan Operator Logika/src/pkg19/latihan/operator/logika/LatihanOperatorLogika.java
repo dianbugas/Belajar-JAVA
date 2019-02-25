456f933db360ce33fe1acec50d1784ea1bca5552
dianynf <dianbugas@gmail.com>
2019-02-25 22:42:17 +0700

    latihan operator logika

---
+++ 19. Latihan Operator Logika/src/pkg19/latihan/operator/logika/LatihanOperatorLogika.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg19.latihan.operator.logika;
+import java.util.*;
+/**
+ *
+ * @author dian
+ */
+public class LatihanOperatorLogika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // membuat sebuah objek untuk menangkap input dari user
+        Scanner inputUser = new Scanner(System.in);
+
+        // sebuah program sederhana untuk menebak sebuah angka
+        int nilaiBenar = 6;
+        int nilaiTebakan;
+        boolean statusTebakan;
+
+        System.out.print("masukan nilai tebakan anda: ");
+        nilaiTebakan = inputUser.nextInt();
+        System.out.println("nilai tebakan anda adalah: " + nilaiTebakan);
+
+        // operasi logika
+        statusTebakan = (nilaiTebakan == nilaiBenar);
+        System.out.println("tebakan anda: " + statusTebakan);
+
+        // operasi aljabar boolean (and / or)
+
+        System.out.print("masukan nilai diantara 4 dan 9: ");
+        nilaiTebakan = inputUser.nextInt();
+
+        statusTebakan = (nilaiTebakan > 4) && (nilaiTebakan < 9);
+
+        /*
+            a | b | c
+            0   0   0
+            0   1   0
+            1   0   0
+            1   1   1
+         */
+
+        System.out.println("tebakan anda: " + statusTebakan);
+
+    }
+    
+}
