cc24d54f6b8a7b709fdb40efe96a996bd8481ccf
dianynf <dianbugas@gmail.com>
2019-09-22 19:50:58 +0700

    operator bitwise

--- 07_OperatorBItwise/src/com/company/Main.java
+++ 07_OperatorBItwise/src/com/company/Main.java
@@ -9,8 +9,23 @@ public class Main {
             byte a=3;
             byte b;
             String a_bits, b_bits;
+            //operator shift left
+            System.out.println("======shift left (<<)");
             a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
             System.out.printf("%s=%d \n", a_bits, a);
+            b = (byte)(a<<3);
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+
+            //operator shift right
+            System.out.println("======shift reght (>>)");
+            a=24;
+            a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", a_bits, a);
+            b = (byte)(a>>2);
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+
 
 
     }
