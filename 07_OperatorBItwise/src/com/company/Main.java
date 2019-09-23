6843db261b5db67acf358ee76b8bc1fb3e20b63a
dianynf <dianbugas@gmail.com>
2019-09-23 14:37:03 +0700

    operator bitwise

--- 07_OperatorBItwise/src/com/company/Main.java
+++ 07_OperatorBItwise/src/com/company/Main.java
@@ -8,7 +8,9 @@ public class Main {
             //operator pada bitwise
             byte a=3;
             byte b;
-            String a_bits, b_bits;
+            byte c;
+
+            String a_bits, b_bits, c_bits;
             //operator shift left
             System.out.println("======shift left (<<)");
             a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
@@ -25,6 +27,69 @@ public class Main {
             b = (byte)(a>>2);
             b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
             System.out.printf("%s=%d \n", b_bits, b);
+
+            //operator bitwise or
+            System.out.println("======== bitwise or (|) ");
+            a= 24;
+            a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", a_bits, a);
+            b = 12;
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+            System.out.println("=============or");
+            c = (byte)(a | b);
+            c_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", c_bits, c);
+
+            //operator bitwise and
+            System.out.println("======== bitwise and (&) ");
+            a= 24;
+            a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", a_bits, a);
+            b = 12;
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+            System.out.println("=============and");
+            c = (byte)(a & b);
+            c_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", c_bits, c);
+
+            //operator bitwise xor
+            System.out.println("======== bitwise xor (^) ");
+            a= 24;
+            a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", a_bits, a);
+            b = 12;
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+            System.out.println("=============xor");
+            c = (byte)(a ^ b);
+            c_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", c_bits, c);
+
+            //operator bitwise and
+            System.out.println("======== bitwise and (&) ");
+            a= 24;
+            a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", a_bits, a);
+            b = 12;
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+            System.out.println("=============and");
+            c = (byte)(a & b);
+            c_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", c_bits, c);
+
+            //operator negasi /not
+            System.out.println("=============bitwise not (~)");
+            a = 24;
+            b = (byte) (~a);
+            a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", a_bits, a);
+            System.out.println("=============not");
+            b_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+            System.out.printf("%s=%d \n", b_bits, b);
+
 
 
 
---
+++ REPEAT/02_string/src/pkg02_string/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg02_string;
+
+/**
+ *
+ * @author dian
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello word" + args[0] + "ganteng");
+        System.out.println("hello word" + args[1] + "manise");
+    }
+}
